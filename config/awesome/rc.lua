b10044cc7ce69c0f0e53fc9ebcd61984bda09e19
Tim Hagemann <tim@infinityblade.de>
2015-11-01 12:48:37 +0100

    Disable annoying tag switching with mouse wheel

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -364,11 +364,11 @@ end
 -- }}}
 
 -- {{{ Mouse Bindings
-root.buttons(awful.util.table.join(
+--[[root.buttons(awful.util.table.join(
     awful.button({ }, 3, function () mymainmenu:toggle() end),
     awful.button({ }, 4, awful.tag.viewnext),
     awful.button({ }, 5, awful.tag.viewprev)
-))
+))]]--
 -- }}}
 
 -- {{{ Key bindings
